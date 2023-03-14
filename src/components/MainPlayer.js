71a51ba2a4db5006fb85e72382523fdbd81b93b4
hillaryke <tenskap@gmail.com>
2023-03-14 21:15:05 +0300

    use showPreview in react-player to return to thumbnail

--- src/components/MainPlayer.js
+++ src/components/MainPlayer.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactPlayer from "react-player";
+import './videoplayer.css';
 
-const VideoPlayer = () => {
+const MainPlayer = () => {
+   const ref = React.useRef(null);
+
+   const handleClick = () => {
+      ref.current.showPreview();
+   }
+
    return (
        <div className="w-full h-full px-16">
           <h2 className="text-4xl font-light text-center py-6">Weddings</h2>
           <p className="text-xl font-light text-center pb-6">We love to capture the love and joy of your special day. We are based in the beautiful city of Cape Town, South Africa. We are available to travel anywhere in the world.</p>
-          <div className="w-full h-[90%]">
+          <div className="w-full h-[90%] relative">
              <ReactPlayer
+                 ref={ref}
                  controls
                  light={<img src='https://www.dropbox.com/s/zceo6ypj26v41fd/IMG_8779%20copy-min.jpg?dl=1' className="w-full h-full object-cover" alt='Thumbnail' />}
                  url="https://player.vimeo.com/video/807676404"
@@ -16,9 +24,11 @@ const VideoPlayer = () => {
              />
           </div>
 
+          <button className="mt-8 px-7 py-4 bg-teal-300" onClick={handleClick}>Stop Playing</button>
+
           <p className="text-xl font-light text-center py-10">We love to capture the love and joy of your special day. We are based in the beautiful city of Cape Town, South Africa. We are available to travel anywhere in the world.</p>
        </div>
    );
 };
 
-export default VideoPlayer;
+export default MainPlayer;
