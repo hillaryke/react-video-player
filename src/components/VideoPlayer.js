b621e7724f8108efa7e0611b7f31f9aa643c4acd
hillaryke <tenskap@gmail.com>
2023-03-14 07:40:24 +0300

    add image thumbnail to videoplayer

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -6,6 +6,7 @@ const VideoPlayer = () => {
        <div className="w-full h-full">
           <ReactPlayer
               controls
+              light={<img src='https://www.dropbox.com/s/zceo6ypj26v41fd/IMG_8779%20copy-min.jpg?dl=1' className="w-full h-full object-cover" alt='Thumbnail' />}
               url="https://player.vimeo.com/video/807676404"
               width="100%"
               height="100%"
