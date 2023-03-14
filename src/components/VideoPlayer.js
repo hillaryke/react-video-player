0603c70205b1754ee6400d8fed2ece88318e4dc4
hillaryke <tenskap@gmail.com>
2023-03-14 07:22:48 +0300

    add width and height to video player

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,8 +3,13 @@ import ReactPlayer from "react-player";
 
 const VideoPlayer = () => {
    return (
-       <div>
-          <ReactPlayer controls url="https://player.vimeo.com/video/807676404" />
+       <div >
+          <ReactPlayer
+              controls
+              url="https://player.vimeo.com/video/807676404"
+              width="100%"
+              height="100%"
+          />
        </div>
    );
 };
