b3a26d1c311c7f1ffff97817418907730fe4c458
hillaryke <tenskap@gmail.com>
2023-03-15 13:09:08 +0300

    exit button to showPreview on click

--- src/components/MainPlayer.js
+++ src/components/MainPlayer.js
@@ -3,6 +3,14 @@ import React from 'react';
 import ReactPlayer from 'react-player/lazy'
 import './MainPlayer.css';
 
+const playIconButton = () => {
+   return (
+       <div id="playButton" className="hover:opacity-50 transition ease-in-out duration-500 hover:cursor-pointer absolute p-7 bg-gray-200 h-[90px] w-[90px] rounded-[100px]"  >
+          <span className="font-light text-lg">Play</span>
+       </div>
+   )
+}
+
 const MainPlayer = () => {
    const [exitButton, setExitButton] = React.useState(null);
    const [playing, setPlaying] = React.useState(false);
@@ -15,12 +23,10 @@ const MainPlayer = () => {
       setExitButton(exitButton);
       // }
    }, [playerReady, exitButton]);
-   
-
 
    const handleShowPreview = () => {
-      exitButton.classList.add('hide');
       exitButton.classList.remove('show');
+      exitButton.classList.add('hide');
       ref.current.showPreview();
    }
 
@@ -32,29 +38,25 @@ const MainPlayer = () => {
       console.log('Clicked Preview');
       console.log(exitButton)
       exitButton.classList.add('show');
+      exitButton.classList.remove('hide');
    }
 
    return (
        <div className="w-full h-full px-24">
           <h2 className="text-4xl font-light text-center py-6">Weddings</h2>
           <p className="text-xl font-light text-center pb-6">We love to capture the love and joy of your special day. We are based in the beautiful city of Cape Town, South Africa. We are available to travel anywhere in the world.</p>
-          <div id="player-container" className="w-full h-[90%] relative">
+          <div id="player-container" className="w-full h-[90%] relative bg-black">
                    <div id="exit-button" className="hover:cursor-pointer show-hide-button p-7 bg-gray-200 h-[90px] w-[90px] rounded-[100px]"
-                     onClick={handlePlayClick}
+                     onClick={handleShowPreview}
                    >
                       <span className="font-light text-lg">Exit</span>
                    </div>
              <ReactPlayer
                  onClickPreview={handleClickPreview}
                  onReady={() => {console.log('onReady'); setPlayerReady(true)}}
-                 // playing={playing}
                  ref={ref}
                  controls
-                 playIcon={<div id="playButton" className="hover:opacity-50 transition ease-in-out duration-500 hover:cursor-pointer absolute p-7 bg-gray-200 h-[90px] w-[90px] rounded-[100px]"
-                                onClick={handlePlayClick}
-                 >
-                    <span className="font-light text-lg">Play</span>
-                 </div>}
+                 playIcon={playIconButton()}
                  light={<img src='https://www.dropbox.com/s/zceo6ypj26v41fd/IMG_8779%20copy-min.jpg?dl=1' className="w-full h-full object-cover" alt='Thumbnail' />}
                  url="https://player.vimeo.com/video/807676404"
                  width="100%"
