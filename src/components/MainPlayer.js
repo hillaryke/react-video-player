3f0bf40095f71573f1ae7b582ed0c69646374782
hillaryke <tenskap@gmail.com>
2023-03-15 10:09:46 +0300

    add black background to player container when starting to play video

--- src/components/MainPlayer.js
+++ src/components/MainPlayer.js
@@ -1,22 +1,52 @@
 import React from 'react';
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
+import ReactPlayer from 'react-player/lazy'
+import './MainPlayer.css';
 
 const MainPlayer = () => {
+   const [playing, setPlaying] = React.useState(false);
+   const [playerReady, setPlayerReady] = React.useState(false);
    const ref = React.useRef(null);
 
+   React.useEffect(() => {
+      if (playerReady) {
+         playerContainer.classList.remove('player-container__darken');
+      }
+   }, []);
+   
+
+   const playerContainer = document.getElementById('player-container');
+
    const handleClick = () => {
+      playerContainer.classList.remove('player-container__darken');
       ref.current.showPreview();
    }
 
+   const handlePlayClick = () => {
+      setPlaying(true);
+   }
+
    return (
-       <div className="w-full h-full px-16">
+       <div className="w-full h-full px-20">
           <h2 className="text-4xl font-light text-center py-6">Weddings</h2>
           <p className="text-xl font-light text-center pb-6">We love to capture the love and joy of your special day. We are based in the beautiful city of Cape Town, South Africa. We are available to travel anywhere in the world.</p>
-          <div className="w-full h-[90%] relative">
-             <div className="show-hide-button absolute p-7 bg-gray-200 h-[90px] w-[90px] rounded-[100px]">Play</div>
+          <div id="player-container" className="w-full h-[90%] relative">
+                   <div className="z-[999] player_icon hover:opacity-50 transition ease-in-out duration-500 hover:cursor-pointer show-hide-button absolute p-7 bg-gray-200 h-[90px] w-[90px] rounded-[100px]"
+                     onClick={handlePlayClick}
+                   >
+                      <span className="font-light text-lg">Exit</span>
+                   </div>
              <ReactPlayer
+                 onClickPreview={() => {console.log('onClickPreview'); playerContainer.classList.add('player-container__darken');}}
+                 onReady={() => {console.log('onReady'); setPlayerReady(true)}}
+                 // playing={playing}
                  ref={ref}
                  controls
+                 playIcon={<div className="z-[999] player_icon hover:opacity-50 transition ease-in-out duration-500 hover:cursor-pointer justify-self-center place-self-center absolute p-7 bg-gray-200 h-[90px] w-[90px] rounded-[100px]"
+                                onClick={handlePlayClick}
+                 >
+                    <span className="font-light text-lg">Play</span>
+                 </div>}
                  light={<img src='https://www.dropbox.com/s/zceo6ypj26v41fd/IMG_8779%20copy-min.jpg?dl=1' className="w-full h-full object-cover" alt='Thumbnail' />}
                  url="https://player.vimeo.com/video/807676404"
                  width="100%"
